1d5380c0270fb44169186e36b84c5f58534cc059
niu.tony.c.zhang <niu.tony.c.zhang@f5a5e4c4-c2b9-54b3-17a4-b3ab355e48aa>
2011-01-20 04:41:02 +0000

    First submit. Setup the package architecture.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import *
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "mainframe-env-simulator",
+    version = "0.0.1",
+    author = "Tony C. Zhang",
+    author_email = "niu.tony.c.zhang@gmail.com",
+    description = ("This program is a Mainframe Programming Environment Simulator running on PC (Linux, Max OS, Windows, etc.) that will compile / assemble source code written to be compiled / assembled on a Mainframe machine. The final goal is to let the developer work on their own PC when developing / testing their programs without worrying about internet connection or connection to a Mainframe machine. It is also aimed at reduce the teaching cost of IBM Assembler, COBOL, etc."),
+    license = "New BSD",
+    keywords = "Simulator mainframe compiler",
+    url = "http://code.google.com/p/mainframe-env-simulator/",
+    packages=find_packages(),
+    scripts = ['zcc'],
+    long_description=read('README'),
+)
---
+++ zPE/core/__init__.py
@@ -0,0 +1,2 @@
+GPR_NUM = 16                    # number of general purpose registers
+MEMORY_SZ = 1048576             # default memory size: 1024K
