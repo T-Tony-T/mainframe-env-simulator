bb88c6649ed16ebdbe3ce583babc542ddc400840
niu.tony.c.zhang <niu.tony.c.zhang@f5a5e4c4-c2b9-54b3-17a4-b3ab355e48aa>
2011-01-20 21:22:02 +0000

    Modified the copyright holder. Fixed the architecture designing.

--- zPE/core/__init__.py
+++ zPE/core/__init__.py
@@ -1,2 +1,4 @@
 GPR_NUM = 16                    # number of general purpose registers
+ADDR_MODE = 31                  # default hardware addressing mode
+ADDR_MAX = 2147483647           # the highest possible 31 bit address
 MEMORY_SZ = 1048576             # default memory size: 1024K
