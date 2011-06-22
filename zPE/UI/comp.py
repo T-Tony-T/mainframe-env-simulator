84dad2eda8814c57d10b62e26c5aebc55cfb05f5
niu.tony.c.zhang <niu.tony.c.zhang@f5a5e4c4-c2b9-54b3-17a4-b3ab355e48aa>
2011-06-22 05:25:00 +0000

    Further improved the split-screen feature. Now support split-on-drag screen creation (not work properly if drag too fast).

--- zPE/UI/comp.py
+++ zPE/UI/comp.py
@@ -6,6 +6,7 @@ pygtk.require('2.0')
 import gtk
 
 
+
 class MainWindow(gtk.Frame):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -30,50 +31,144 @@ class MainWindow(gtk.Frame):
         root_frame = gtk.Table(3, 3, False)
         self.add(root_frame)
 
-        self.bttn_lt = gtk.Button()
-        self.bttn_rt = gtk.Button()
-        self.bttn_tp = gtk.Button()
-        self.bttn_bm = gtk.Button()
+        self.ctrl_paned = {}
+        self.ctrl_paned_id = {}
+
+        self.ctrl_paned['lt'] = gtk.Frame()
+        paned = gtk.HPaned()
+        dummy = ( gtk.Layout(), gtk.Layout() )
+        paned.pack1(dummy[0], False, True)
+        paned.pack2(dummy[1], False, True)
+        dummy[0].set_size_request(1, -1)
+        dummy[1].set_size_request(1, -1)
+        self.ctrl_paned['lt'].add(paned)
+
+        self.ctrl_paned['rt'] = gtk.Frame()
+        paned = gtk.HPaned()
+        dummy = ( gtk.Layout(), gtk.Layout() )
+        paned.pack1(dummy[0], False, True)
+        paned.pack2(dummy[1], False, True)
+        dummy[0].set_size_request(1, -1)
+        dummy[1].set_size_request(1, -1)
+        self.ctrl_paned['rt'].add(paned)
+
+        self.ctrl_paned['tp'] = gtk.Frame()
+        paned = gtk.VPaned()
+        dummy = ( gtk.Layout(), gtk.Layout() )
+        paned.pack1(dummy[0], False, True)
+        paned.pack2(dummy[1], False, True)
+        dummy[0].set_size_request(-1, 1)
+        dummy[1].set_size_request(-1, 1)
+        self.ctrl_paned['tp'].add(paned)
+
+        self.ctrl_paned['bm'] = gtk.Frame()
+        paned = gtk.VPaned()
+        dummy = ( gtk.Layout(), gtk.Layout() )
+        paned.pack1(dummy[0], False, True)
+        paned.pack2(dummy[1], False, True)
+        dummy[0].set_size_request(-1, 1)
+        dummy[1].set_size_request(-1, 1)
+        self.ctrl_paned['bm'].add(paned)
+
         self.mw_center = gtk.Frame()
         self.mw_center.add(MainWindowFrame())
+        paned = None            # clear dummy var
 
-        root_frame.attach(self.bttn_lt, 0, 1, 1, 2, xoptions=0)
-        root_frame.attach(self.bttn_rt, 2, 3, 1, 2, xoptions=0)
-        root_frame.attach(self.bttn_tp, 1, 2, 0, 1, yoptions=0)
-        root_frame.attach(self.bttn_bm, 1, 2, 2, 3, yoptions=0)
-        root_frame.attach(self.mw_center, 1, 2, 1, 2)
+        root_frame.attach(self.ctrl_paned['lt'], 0, 1, 1, 2, xoptions=gtk.SHRINK)
+        root_frame.attach(self.ctrl_paned['rt'], 2, 3, 1, 2, xoptions=gtk.SHRINK)
+        root_frame.attach(self.ctrl_paned['tp'], 1, 2, 0, 1, yoptions=gtk.SHRINK)
+        root_frame.attach(self.ctrl_paned['bm'], 1, 2, 2, 3, yoptions=gtk.SHRINK)
+        root_frame.attach(self.mw_center,        1, 2, 1, 2)
 
-        self.bttn_lt.connect_object("clicked", self.add_frame, self.mw_center, "lt")
-        self.bttn_rt.connect_object("clicked", self.add_frame, self.mw_center, "rt")
-        self.bttn_tp.connect_object("clicked", self.add_frame, self.mw_center, "tp")
-        self.bttn_bm.connect_object("clicked", self.add_frame, self.mw_center, "bm")
+        self.ctrl_paned_id['lt'] = self.ctrl_paned['lt'].child.connect(
+            'notify', self._sig_ctrl_move, self.mw_center, 'lt'
+            )
+        self.ctrl_paned_id['rt'] = self.ctrl_paned['rt'].child.connect(
+            'notify', self._sig_ctrl_move, self.mw_center, 'rt'
+            )
+        self.ctrl_paned_id['tp'] = self.ctrl_paned['tp'].child.connect(
+            'notify', self._sig_ctrl_move, self.mw_center, 'tp'
+            )
+        self.ctrl_paned_id['bm'] = self.ctrl_paned['bm'].child.connect(
+            'notify', self._sig_ctrl_move, self.mw_center, 'bm'
+            )
 
+
+    def _sig_ctrl_move(self, widget, param_spec, target, pos):
+        valid_exec = (param_spec.name == 'position')
+        if not valid_exec:      # skip non-move action
+            return
+        try:
+            if widget.__sig_ctrl_move_called != None and valid_exec:
+                widget.__sig_ctrl_move_called += 1
+        except:
+            widget.__sig_ctrl_move_called = 0
+
+        if widget.__sig_ctrl_move_called < 1: # skip the first run (on creation)
+            return
+
+        if pos in [ 'lt', 'rt', 'tp', 'bm' ]:
+            paned = self.add_frame(target, pos) # target should be self.mw_center
 
 
     def add_frame(self, parent, pos):
         child = parent.child
-        if pos in ("lt", "rt"):
-            paned = gtk.HPaned()
-        else:
-            paned = gtk.VPaned()
-        parent.remove(child)
-        parent.add(paned)
-        if pos in ("lt", "tp"):
-            paned.pack1(MainWindowFrame(), True, False)
-            paned.pack2(child, True, False)
-        else:
-            paned.pack1(child, True, False)
-            paned.pack2(MainWindowFrame(), True, False)
-        parent.show_all()
 
+        # retrive the ctrl paned and its parent
+        paned_parent = self.ctrl_paned[pos]
+        paned = self.ctrl_paned[pos].child
+
+        # create new ctrl paned
+        if pos in ('lt', 'rt'):
+            new_paned = gtk.HPaned()
+        else:
+            new_paned = gtk.VPaned()
+
+        # re-connect signals
+        paned.disconnect(self.ctrl_paned_id[pos])
+        paned.connect('notify', self._sig_ctrl_move, self.mw_center, 'ct')
+        self.ctrl_paned_id[pos] = new_paned.connect(
+            'notify', self._sig_ctrl_move, self.mw_center, pos
+            )
+
+        # re-parent the widgets
+        child_1 = paned.get_child1()
+        child_2 = paned.get_child2()
+        paned.remove(child_1)
+        paned.remove(child_2)
+        new_paned.pack1(child_1, False, True)
+        new_paned.pack2(child_2, False, True)
+
+        parent.remove(child)
+        paned_parent.remove(paned)
+
+        if pos in ('lt', 'tp'):
+            paned.pack1(MainWindowFrame(), True, True)
+            paned.pack2(child, True, True)
+        else:
+            paned.pack1(child, True, True)
+            paned.pack2(MainWindowFrame(), True, True)
+
+        parent.add(paned)
+        paned_parent.add(new_paned)
+
+        # show all
+        paned.set_position(-1)
+        parent.show_all()
+        paned_parent.show_all()
+        return paned
 
 
 class MainWindowFrame(gtk.VBox):
-    def __init__(self):
+    def __init__(self, widget = None):
         super(MainWindowFrame, self).__init__()
 
-        self.center = gtk.Frame()
+        if widget:
+            self.center = widget
+        else:
+            self.center = gtk.Label("TEST")
+
         self.pack_start(self.center, True, True, 0)
 
-        self.bottom = gtk.Button("test")
+        self.bottom = gtk.Button('test')
         self.pack_end(self.bottom, False, False, 0)
