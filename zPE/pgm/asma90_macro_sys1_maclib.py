34938a709061d0cf73be2adc956eced8114f12fa
niu.tony.c.zhang <niu.tony.c.zhang@f5a5e4c4-c2b9-54b3-17a4-b3ab355e48aa>
2012-01-26 05:34:35 +0000

    added the macro lib definition

---
+++ zPE/pgm/asma90_macro_sys1_maclib.py
@@ -0,0 +1,27 @@
+MACRO_DEF = {
+    # X-Macros
+    'XDUMP'  : lambda args: [
+        ],
+
+    # IBM Structured Macros
+    'IF'     : lambda args: [
+        ],
+    'ELSEIF' : lambda args: [
+        ],
+    'ELSE'   : lambda: [
+        ],
+    'ENDIF'  : lambda: [
+        ],
+
+    'SELECT' : lambda args: [
+        ],
+    'WHEN'   : lambda args: [
+        ],
+    'ENDSEL' : lambda: [
+        ],
+
+    'DO'     : lambda args: [
+        ],
+    'ENDDO'  : lambda: [
+        ],
+    }
